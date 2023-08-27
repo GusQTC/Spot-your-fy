bc3a7d43f4cdfbf24831a609dc0c2098297be3e3
Gustavo Castro <gustavoqt@usp.br>
2023-08-27 17:43:06 -0300

    Added webpage

--- spotify.py
+++ spotify.py
@@ -1,33 +1,52 @@
 import configparser
-import requests
 import spotipy
-import sys
 from spotipy.oauth2 import SpotifyClientCredentials
+import json
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import Blueprint
 
-# load the mongo_config values
+app = Flask(__name__)
+
 parser = configparser.ConfigParser()
-parser.read("Scripts\pipeline.conf")
+parser.read("Spot-your-fy/spotify.conf")
 client_id = parser.get("spotify_creds", "client_id")
 client_secret = parser.get("spotify_creds", "client_secret")
 
-name = input("Artist Name: ")
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
 
-results_artists = sp.search(q='artist:' + name, type='artist')
-items = results_artists['artists']['items']
-if len(items) > 0:
-    artist = items[0]
-    uri = artist['external_urls']['spotify']
-    print(uri)
+@app.route('/')
+def my_form():
+    return render_template("spot.html") # This should be the name of your HTML file
 
-results = sp.artist_albums(uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = sp.next(results)
-    albums.extend(results['items'])
+@app.route('/', methods=['POST'])
+def my_form_post():
+    name = request.form['artist']
+    results_artists = sp.search(q='artist:' + name, type='artist')
+    items = results_artists['artists']['items']
+    if len(items) > 0:
+        artist = items[0]
+        uri = artist['external_urls']['spotify']
 
-for album in albums:
-    print(album['name'])
+    results = sp.artist_albums(uri, album_type='album')
+    albums = results['items']
+    while results['next']:
+        results = sp.next(results)
+        albums.extend(results['items'])
+
+    album_names = []
+    for album in albums:
+        album_names.append(album['name'])
+    return render_template('albums.html', album_names=album_names, artist_name = name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# load the mongo_config values
+
+
+
 
 '''
 AUTH_URL = 'https://accounts.spotify.com/api/token'
