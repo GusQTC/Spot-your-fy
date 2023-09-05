0c50546a200bea41572a577fbe01d41f61e1a3ce
Gustavo Castro <gustavoqt@usp.br>
2023-09-05 14:14:38 -0300

    Changed from user to recomendations

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from spotipy import oauth2
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask, request, url_for, session, redirect, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -13,100 +14,79 @@ client_id = parser.get("spotify_creds", "client_id")
 client_secret = parser.get("spotify_creds", "client_secret")
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
 
-scope_user_top = "user-top-read"
-scope_user_playlists = "playlist-read-private"
-
 sp_oauth = oauth2.SpotifyOAuth(
     client_id=client_id,
     client_secret=client_secret,
-    redirect_uri='http://localhost:5000/spotify/callback',
+    redirect_uri='https://spot-your-fy.uc.r.appspot.com/',
     scope=['user-top-read']
 )
 
 sp_user_top = spotipy.Spotify(auth_manager=sp_oauth)
 sp_auth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, 
-                        redirect_uri="https://accounts.spotify.com",scope=scope_user_playlists)
+                        redirect_uri="https://spot-your-fy.uc.r.appspot.com/",scope=['playlist-read-private'])
 sp_user_playlists = spotipy.Spotify(auth_manager=sp_auth)
 
 
-@app.route('/login')
-def login():
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)
-
 @app.route('/')
 def index():
-    token_info = session.get("token_info", None)
-    if not token_info:
-        return redirect(url_for('login'))
-    else:
         return render_template("index.html")
 
-@app.route("/spotify/callback")
-def spotify_callback():
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-    session["token_info"] = token_info
-    return redirect('/')
+@app.route('/', methods=['GET', 'POST'])
+def functions():
 
+    if request.form.get('get_followers') == 'Check Followers!':
 
-@app.route('/form', methods=['GET','POST'])
-def my_form_post():
-    token_info = session.get("token_info", None)
-    # If there's no token in the session, redirect user to login
-    if not token_info:
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    else:
-
-        if request.form.get('get_albums') == 'Check Albums!':
-            name = request.form['artist']
-            results_artists = sp.search(q='artist:' + name, type='artist')
-            items = results_artists['artists']['items']
-            if len(items) > 0:
-                artist = items[0]
-                uri = artist['external_urls']['spotify']
-
-            results = sp.artist_albums(uri, album_type='album')
-            albums = results['items']
-            while results['next']:
-                results = sp.next(results)
-                albums.extend(results['items'])
-
-            album_names = []
-            for album in albums:
-                album_names.append(album['name'])
-            return render_template('albums.html', album_names=album_names, artist_name = name)
+        artist = request.form['artist']
+        results_artists = sp.search(q='artist:' + artist, type='artist')
+        items = results_artists['artists']['items']
+        if len(items) > 0:
+            artist = items[0]
+            followers = artist['followers']['total']
+        return render_template('followers.html', followers=followers, artist_name = artist)
     
-        if request.form.get('get_followers') == 'Check Followers!':
-            name = request.form['artist']
-            results_artists = sp.search(q='artist:' + name, type='artist')
-            items = results_artists['artists']['items']
-            if len(items) > 0:
-                artist = items[0]
-                uri = artist['external_urls']['spotify']
-                followers = artist['followers']['total']
-                return render_template('followers.html', followers=followers, artist_name = name)
-        
-        if request.form.get('get_tracks') == 'Check Tracks!':
-            results = sp_user_top.current_user_top_tracks()
-            user = sp_user_top.current_user()
+    if request.form.get('get_recs') == 'Get Recommendations':
+        track_ex = request.form['track_ex']
+        artist_ex = request.form['art_ex']
 
-            tracks = results['items']
-            track_names = []
-            for track in tracks:
-                track_names.append(track['name'])
-            return render_template('top_tracks.html', track_names=track_names, user = user)
-        
-        if request.form.get('get_playlists') == 'Check Playlists!':
-            results = sp_user_playlists.current_user_playlists()
-            user = sp.current_user()
 
-            playlists = results['items']
-            playlist_names = []
-            for playlist in playlists:
-                playlist_names.append(playlist['name'])
-            return render_template('playlists.html', playlist_names=playlist_names, user = user)
+        results_artists = sp.search(q='artist:' + artist_ex, type='artist')
+        items = results_artists['artists']['items']
+        if len(items) > 0:
+            artist = items[0]
+            uri_artist = artist['external_urls']['spotify']
 
+        results_track = sp.search(q='track:' + track_ex, type='track')
+        items = results_track['tracks']['items']
+        if len(items) > 0:
+            track = items[0]
+            uri_track = track['external_urls']['spotify']
+
+        result = sp.recommendations(seed_artists=[uri_artist], seed_tracks=[uri_track], limit=10)
+        recomendations = result['tracks']
+        song_names = []
+        for track in recomendations:
+            song_names.append(track['name'])
+        return render_template('recs.html', result = song_names)
+
+
+    if request.form.get('get_albums') == 'Check Albums!':
+        artist = request.form['artist']
+        results_artists = sp.search(q='artist:' + artist, type='artist')
+        items = results_artists['artists']['items']
+        if len(items) > 0:
+            artist = items[0]
+            uri = artist['external_urls']['spotify']
+
+        results = sp.artist_albums(uri, album_type='album')
+        albums = results['items']
+        while results['next']:
+            results = sp.next(results)
+            albums.extend(results['items'])
+
+        album_names = []
+        for album in albums:
+            album_names.append(album['name'])
+        return render_template('albums.html', album_names=album_names, artist_name = artist)
+                        
 if __name__ == '__main__':
     app.run()
