290007303ed903d1d1a07183d955da8d335d9a1b
Gustavo Torres <133672798+GusQTC@users.noreply.github.com>
2023-09-28 19:17:06 -0300

    Revert "Added input sanitization~" Redid to the previous state, to branch This reverts commit f8653c08b296a21241f888e8978c257a48b44089.

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import configparser
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from spotipy.oauth2 import SpotifyOAuth
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, url_for, session, redirect, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -26,17 +25,50 @@ sp_user_playlists = spotipy.Spotify(auth_manager=sp_auth)
 
 
 @app.route('/')
-def my_form():
-    return render_template("index.html") # This should be the name of your HTML file
+def index():
+        return render_template("index.html")
 
+@app.route('/', methods=['GET', 'POST'])
+def functions():
 
-@app.route('/', methods=['GET','POST'])
-def my_form_post():
+    if request.form.get('get_followers') == 'Check Followers!':
+
+        artist = request.form['artist']
+        results_artists = sp.search(q='artist:' + artist, type='artist')
+        items = results_artists['artists']['items']
+        if len(items) > 0:
+            artist = items[0]
+            followers = artist['followers']['total']
+        return render_template('followers.html', followers=followers, artist_name = artist)
     
+    if request.form.get('get_recs') == 'Get Recommendations':
+        track_ex = request.form['track_ex']
+        artist_ex = request.form['art_ex']
+
+
+        results_artists = sp.search(q='artist:' + artist_ex, type='artist')
+        items = results_artists['artists']['items']
+        if len(items) > 0:
+            artist = items[0]
+            uri_artist = artist['external_urls']['spotify']
+
+        results_track = sp.search(q='track:' + track_ex, type='track')
+        items = results_track['tracks']['items']
+        if len(items) > 0:
+            track = items[0]
+            uri_track = track['external_urls']['spotify']
+
+        result = sp.recommendations(seed_artists=[uri_artist], seed_tracks=[uri_track], limit=10)
+        recomendations = result['tracks']
+        song_names = []
+        for track in recomendations:
+            song_names.append(track['name'])
+        return render_template('recs.html', result = song_names)
+
 
     if request.form.get('get_albums') == 'Check Albums!':
-        name = request.form['artist']
-        results_artists = sp.search(q='artist:' + name, type='artist')
+        artist = request.form['artist']
+        results_artists = sp.search(q='artist:' + artist, type='artist')
         items = results_artists['artists']['items']
         if len(items) > 0:
             artist = items[0]
